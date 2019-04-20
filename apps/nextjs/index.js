2bd9dc6d99350c71b91971c5bdeb2306c00abb29
Samuele Di Rito <samuele.dirito@gmail.com>
2019-04-20 11:41:26 +0200

    Next js Application (does not work)

---
+++ apps/nextjs/index.js
@@ -0,0 +1,6 @@
+const sls = require('serverless-http');
+const binaryMimeTypes = require('./binaryMimeTypes');
+
+const server = require('./server');
+
+module.exports.server = sls(server, { binary: binaryMimeTypes });
